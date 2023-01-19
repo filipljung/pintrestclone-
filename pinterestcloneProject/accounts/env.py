ed6ef5ab5e41ed065b16378198ec1bc1ffdfa7ae
filipljung <96741967+filipljung@users.noreply.github.com>
2023-01-19 10:11:49 +0100

    added database url

---
+++ pinterestcloneProject/accounts/env.py
@@ -0,0 +1,1 @@
+os.environ.setdefault("DATABASE_URL", "postgres://wigegpni:vYN1LXnLfkDOMBT-HaaQk9PImu8nRL1R@mel.db.elephantsql.com/wigegpni")
--- pinterestcloneProject/settings.py
+++ pinterestcloneProject/settings.py
@@ -1,5 +1,7 @@
-import os
 from pathlib import Path
+ import os
+ import dj_database_url
+ import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -69,12 +71,17 @@ AUTH_USER_MODEL = 'accounts.user'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {#
+#  'default': {
+#      'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+ #    }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default' : dj_database_url.parse(os.version.get('DATABASE_URL'))
 }
+
 
 
 # Password validation
