ed22417437a9175f33e4ba48f019bc1846b6d927
filipljung <96741967+filipljung@users.noreply.github.com>
2023-01-24 10:32:17 +0100

    changes

--- pinterestclone/pinterest_clone/settings.py
+++ pinterestclone/pinterest_clone/settings.py
@@ -1,5 +1,7 @@
-import os
-from pathlib import Path
+ from pathlib import Path
+ import os
+ import dj_database_url
+ import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
