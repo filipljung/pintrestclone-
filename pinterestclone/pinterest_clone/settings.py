b946fd68a2e9dab6e46aaf821b7bbec96a053c08
filipljung <96741967+filipljung@users.noreply.github.com>
2023-01-24 10:30:49 +0100

    changes

--- pinterestclone/pinterest_clone/settings.py
+++ pinterestclone/pinterest_clone/settings.py
@@ -69,12 +69,12 @@ AUTH_USER_MODEL = 'accounts.user'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#    'default': {
+#       'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+# }
 
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get('postgres://wigegpni:vYN1LXnLfkDOMBT-HaaQk9PImu8nRL1R@mel.db.elephantsql.com/wigegpni'))
