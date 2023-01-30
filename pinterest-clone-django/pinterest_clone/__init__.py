59b3922f3862b616b4f310c138ee5bbd4fdfaf93
filipljung <96741967+filipljung@users.noreply.github.com>
2023-01-30 15:56:22 +0100

    added init database

--- pinterest-clone-django/pinterest_clone/__init__.py
+++ pinterest-clone-django/pinterest_clone/__init__.py
@@ -1,11 +1,3 @@
-app = Pinterest-clone-django(__name__)
- app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-
- if os.environ.get("DEVELOPMENT") == "True":
-     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
- else:
-     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
-
  if os.environ.get("DEVELOPMENT") == "True":
      app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
  else:
